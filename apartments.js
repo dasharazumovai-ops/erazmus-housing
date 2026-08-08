977ac7b3af12c3fe93329686b0171653e66f93c2
dasharazumovai-ops <dasharazumova.i@gmail.com>
2026-08-08 10:02:35 +0300

    fixed

--- apartments.js
+++ apartments.js
@@ -2869,7 +2869,7 @@ id: 63,
 
   sharedSpaces: "Fully equipped kitchen, comfortable living area, balcony and bathroom. Utilities (electricity & water) are shared between tenants.",
 
-  location: "https://maps.app.goo.gl/2TNvtV8RFibQ2Z57?g_st=ic",
+  location: "https://maps.app.goo.gl/KtszGALQb67BZYkP8",
 
   gallery: [
     "images/aglantzia/apt66/1.jpg",
